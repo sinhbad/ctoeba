944e290e9289054466ac77410dc1e821fa099521
sinhbad <sinhbad@yandex.ru>
2015-11-16 20:39:55 +0300

    modified:   ctoeba/install.sh

--- ctoeba/install.sh
+++ ctoeba/install.sh
@@ -5,4 +5,5 @@ echo "Enter ./tcv-no-color3 <level> <count> <lc1> <lc2> for showing [count] resu
 echo "For example:"
 echo "./tcv-no-color3 5 100 rus eng"
 echo "./tcv-no-color3 5 100 eng deu"
+echo "if it dowsn't work try python3 tcv-no-color3 5 100 eng deu"
 echo 
