2d1df0a7714429d62e8f21fd74ff0577c2ec176d
sinhbad <sinhbad@yandex.ru>
2015-11-16 20:24:17 +0300

    deleted:    ctoeba/init.sql 	modified:   ctoeba/install.sh 	deleted:    ctoeba/lower.py 	deleted:    ctoeba/separator.awk 	modified:   ctoeba/tcv-no-color3

--- ctoeba/install.sh
+++ ctoeba/install.sh
@@ -1,26 +1,8 @@
-wget http://downloads.tatoeba.org/exports/sentences.tar.bz2
-wget http://downloads.tatoeba.org/exports/links.tar.bz2
-echo "Extract sentences.csv..."
-tar xvjf sentences.tar.bz2
-echo "Ready!"
-echo "Extract links.csv..."
-tar xvjf links.tar.bz2
-echo "Ready!"
-echo "sentences.csv processing"
-awk -f separator.awk sentences.csv > sentences_awk.csv
-echo "Ready!"
-echo "Lowercase conversion..."
-./lower.py sentences_awk.csv sentencesl.csv
-echo "Ready!"
-echo "Creating database"
-sqlite3 < init.sql
-echo "Ready!"
-echo "Cleaning..."
-ls | grep bz2 | xargs rm -rf
-ls | grep csv | xargs rm -rf
+wget http://folkcovers.org/static/tatoeba.db
 echo "Ready!"
 echo "Installation completed successfully"
 echo "Enter ./tcv-no-color3 <level> <count> <lc1> <lc2> for showing [count] results from [lc1] to [lc2] with depth [level]"
 echo "For example:"
 echo "./tcv-no-color3 5 100 rus eng"
 echo "./tcv-no-color3 5 100 eng deu"
+echo 
